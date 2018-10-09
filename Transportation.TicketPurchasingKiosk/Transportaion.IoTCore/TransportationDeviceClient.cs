5616ef0ee712245b71f6916312a2f4613878ec76
Carl Schweitzer <Carl.Schweitzer@microsoft.com>
2018-10-09 15:02:50 -0700

    Add initial Send/Receive methods
    Also corrected mis-spelling in project name.

--- Transportation.TicketPurchasingKiosk/Transportaion.IoTCore/TransportationDeviceClient.cs
+++ Transportation.TicketPurchasingKiosk/Transportaion.IoTCore/TransportationDeviceClient.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Transportaion.IoTCore
+namespace Transportation.IoTCore
 {
     public class TransportationDeviceClient
     {
+        private DeviceClient deviceClient;
+        public TransportationDeviceClient(string connectionString)
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+        }
+
+        public async Task SendMessageAsync(string msg)
+        {
+            var message = new Message(msg.GetBytes());
+            deviceClient.SendEventAsync(message);
+        }
+
+        public async Task SendMessageBatchAsync(IEnumerable<string> msgs)
+        {
+            var messages = new List<Message>();
+            foreach (var item in msgs)
+            {
+                messages.Add(new Message(item.GetBytes()));
+            }
+            deviceClient.SendEventBatchAsync(messages);
+        }
+        
+        public async Task<Message> ReceiveMessageAsync()
+        {
+            var message = await deviceClient.ReceiveAsync(TimeSpan.FromSeconds(60 * 1));
+            return message;
+        }
+        
     }
 }
