bb7b1d2a35943f98f1882486c0be1af9f7c42b54
Rajasa Savant <rasavant@microsoft.com>
2018-10-11 12:56:16 -0700

    Low Stock Action Logic function Added

---
+++ src/Functions/TransitFunctionApp/LowStockFunction.cs
@@ -0,0 +1,33 @@
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+using Transportation.Demo.Shared.Models;
+
+namespace Transportation.Demo.Functions
+{
+    public static class LowStockFunction
+    {
+        [FunctionName("LowStockFunction")]
+        public static void Run(
+            [
+            EventHubTrigger("lowstockeventhub", 
+            Connection = "receiverConnectionString")
+            ]
+        EventData[] eventHubMessages, ILogger log)
+        {
+            foreach (var message in eventHubMessages)
+            {
+                string messagePayload = Encoding.UTF8.GetString(message.Body.Array);
+
+                LowStockRequest ticketRequestMessage = JsonConvert.DeserializeObject<LowStockRequest>(messagePayload);
+
+                log.LogWarning($" !!!!! Low Stock Alert !!!! \n\t DeviceID - {ticketRequestMessage.DeviceId} " +
+                    $"\n\t Time - {ticketRequestMessage.CreateTime}" +
+                    $"\n\t Current Stock Level - {ticketRequestMessage.StockLevel}");
+            }
+        }
+    }
+}
