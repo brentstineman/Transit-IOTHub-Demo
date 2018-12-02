4d732a126ef874394ed57386d357f03d073e6aea
Sarah Aly <hms4ever@live.com>
2018-12-02 00:31:20 -0800

    TODO: Set Device Status

--- src/Shared/Models/IDeviceConfig.cs
+++ src/Shared/Models/IDeviceConfig.cs
@@ -2,9 +2,11 @@
 
 namespace Transportation.Demo.Shared.Models
 {
+    
     public interface IDeviceConfig
     {
         string DeviceId { get; set; }
         string DeviceType { get; set; }
+
     }
 }
